85cace8bac41545437ab34717c7a41638e1e4569
Tavis Palmer <tavis@tavispalmer.com>
2025-11-09 13:46:49 -0500

    add arithmetic/branch/flag instructions

--- src/cpu.rs
+++ src/cpu.rs
@@ -57,96 +57,296 @@ impl Cpu {
         (self.ps & Self::CARRY) != 0
     }
     #[inline]
-    const fn clear_carry(&mut self) {
-        self.ps &= !Self::CARRY
-    }
-    #[inline]
-    const fn set_carry(&mut self) {
-        self.ps |= Self::CARRY
+    const fn set_carry(&mut self, carry: bool) {
+        if carry {
+            self.ps |= Self::CARRY
+        } else {
+            self.ps &= !Self::CARRY
+        }
     }
     #[inline]
     const fn zero(&self) -> bool {
         (self.ps & Self::ZERO) != 0
     }
     #[inline]
-    const fn clear_zero(&mut self) {
-        self.ps &= !Self::ZERO
-    }
-    #[inline]
-    const fn set_zero(&mut self) {
-        self.ps |= Self::ZERO
+    const fn set_zero(&mut self, zero: bool) {
+        if zero {
+            self.ps |= Self::ZERO
+        } else {
+            self.ps &= !Self::ZERO
+        }
     }
     #[inline]
     const fn irq_disable(&self) -> bool {
         (self.ps & Self::IRQ_DISABLE) != 0
     }
     #[inline]
-    const fn clear_irq_disable(&mut self) {
-        self.ps &= !Self::IRQ_DISABLE
-    }
-    #[inline]
-    const fn set_irq_disable(&mut self) {
-        self.ps |= Self::IRQ_DISABLE
+    const fn set_irq_disable(&mut self, irq_disable: bool) {
+        if irq_disable {
+            self.ps |= Self::IRQ_DISABLE
+        } else {
+            self.ps &= !Self::IRQ_DISABLE
+        }
     }
     #[inline]
     const fn decimal(&self) -> bool {
         (self.ps & Self::DECIMAL) != 0
     }
     #[inline]
-    const fn clear_decimal(&mut self) {
-        self.ps &= !Self::DECIMAL
-    }
-    #[inline]
-    const fn set_decimal(&mut self) {
-        self.ps |= Self::DECIMAL
+    const fn set_decimal(&mut self, decimal: bool) {
+        if decimal {
+            self.ps |= Self::DECIMAL
+        } else {
+            self.ps &= !Self::DECIMAL
+        }
     }
     #[inline]
     const fn index_mode_8(&self) -> bool {
         (self.ps & Self::INDEX_MODE_8) != 0
     }
     #[inline]
-    const fn set_index_mode_16(&mut self) {
-        self.ps &= !Self::INDEX_MODE_8
-    }
-    #[inline]
-    const fn set_index_mode_8(&mut self) {
-        self.ps |= Self::INDEX_MODE_8
+    const fn set_index_mode_8(&mut self, index_mode_8: bool) {
+        if index_mode_8 {
+            self.ps |= Self::INDEX_MODE_8
+        } else {
+            self.ps &= !Self::INDEX_MODE_8
+        }
     }
     #[inline]
     const fn memory_mode_8(&self) -> bool {
         (self.ps & Self::MEMORY_MODE_8) != 0
     }
     #[inline]
-    const fn set_memory_mode_16(&mut self) {
-        self.ps &= !Self::MEMORY_MODE_8
-    }
-    #[inline]
-    const fn set_memory_mode_8(&mut self) {
-        self.ps |= Self::MEMORY_MODE_8
+    const fn set_memory_mode_8(&mut self, memory_mode_8: bool) {
+        if memory_mode_8 {
+            self.ps |= Self::MEMORY_MODE_8
+        } else {
+            self.ps &= !Self::MEMORY_MODE_8
+        }
     }
     #[inline]
     const fn overflow(&self) -> bool {
         (self.ps & Self::OVERFLOW) != 0
     }
     #[inline]
-    const fn clear_overflow(&mut self) {
-        self.ps &= !Self::OVERFLOW
-    }
-    #[inline]
-    const fn set_overflow(&mut self) {
-        self.ps |= Self::OVERFLOW
+    const fn set_overflow(&mut self, overflow: bool) {
+        if overflow {
+            self.ps |= Self::OVERFLOW
+        } else {
+            self.ps &= !Self::OVERFLOW
+        }
     }
     #[inline]
     const fn negative(&self) -> bool {
         (self.ps & Self::NEGATIVE) != 0
     }
     #[inline]
-    const fn clear_negative(&mut self) {
-        self.ps &= !Self::NEGATIVE
+    const fn set_negative(&mut self, negative: bool) {
+        if negative {
+            self.ps |= Self::NEGATIVE
+        } else {
+            self.ps &= !Self::NEGATIVE
+        }
     }
+
     #[inline]
-    const fn set_negative(&mut self) {
-        self.ps |= Self::NEGATIVE
+    const fn set_zero_negative8(&mut self, value: u8) {
+        self.set_zero(value == 0);
+        self.set_negative((value as i8) < 0);
+    }
+
+    #[inline]
+    const fn set_zero_negative16(&mut self, value: u16) {
+        self.set_zero(value == 0);
+        self.set_negative((value as i16) < 0);
+    }
+
+    // instructions
+    const fn adc8(&mut self, rhs: u8) {
+        let mut result;
+        if self.decimal() {
+            result = (self.a & 0x0f) + (rhs as u16 & 0x0f) + self.carry() as u16;
+
+            if result > 0x09 { result += 0x06 }
+            result = (self.a & 0xf0) + (rhs as u16 & 0xf0) + if result > 0x0f { 0x10 } else { 0 } + (result & 0x0f);
+        } else {
+            result = self.a + rhs as u16 + self.carry() as u16;
+        }
+
+        self.set_overflow((!(self.a as u8 ^ rhs) & (self.a as u8 ^ result as u8) & 0x80) != 0);
+
+        if self.decimal() && result > 0x9f {
+            result += 0x60
+        }
+
+        self.set_zero_negative8(result as u8);
+        self.set_carry(result > 0xff);
+
+        self.a = (self.a & 0xff00) | (result & 0xff)
+    }
+    const fn adc16(&mut self, rhs: u16) {
+        let mut result;
+        if self.decimal() {
+            result = (self.a as u32 & 0x0f) + (rhs as u32 & 0x0f) + self.carry() as u32;
+
+            if result > 0x09 { result += 0x06 }
+            result = (self.a as u32 & 0xf0) + (rhs as u32 & 0xf0) + if result > 0x0f { 0x10 } else { 0 } + (result as u32 & 0x0f);
+
+            if result > 0x9f { result += 0x60 }
+            result = (self.a as u32 & 0xf00) + (rhs as u32 & 0xf00) + if result > 0xff { 0x100 } else { 0 } + (result as u32 & 0xff);
+
+            if result > 0x9ff { result += 0x600 }
+            result = (self.a as u32 & 0xf000) + (rhs as u32 & 0xf000) + if result > 0xfff { 0x1000 } else { 0 } + (result as u32 & 0xfff);
+        } else {
+            result = self.a as u32 + rhs as u32 + self.carry() as u32;
+        }
+
+        self.set_overflow((!(self.a ^ rhs) & (self.a ^ result as u16) & 0x8000) != 0);
+
+        if self.decimal() && result > 0x9fff {
+            result += 0x6000
+        }
+
+        self.set_zero_negative16(result as u16);
+        self.set_carry(result > 0xffff);
+
+        self.a = result as u16
+    }
+    const fn adc(&mut self, rhs: u16) {
+        if self.memory_mode_8() {
+            self.adc8(rhs as u8)
+        } else {
+            self.adc16(rhs as u16)
+        }
+    }
+
+    const fn sbc8(&mut self, rhs: u8) {
+        let rhs = !rhs;
+        let mut result;
+        if self.decimal() {
+            result = (self.a & 0x0f) + (rhs as u16 & 0x0f) + self.carry() as u16;
+
+            if result <= 0x0f { result -= 0x06 }
+            result = (self.a & 0xf0) + (rhs as u16 & 0xf0) + if result > 0x0f { 0x10 } else { 0 } + (result & 0x0f);
+        } else {
+            result = self.a + rhs as u16 + self.carry() as u16;
+        }
+
+        self.set_overflow((!(self.a as u8 ^ rhs) & (self.a as u8 ^ result as u8) & 0x80) != 0);
+
+        if self.decimal() && result <= 0xff {
+            result -= 0x60
+        }
+
+        self.set_zero_negative8(result as u8);
+        self.set_carry(result > 0xff);
+
+        self.a = (self.a & 0xff00) | (result & 0xff)
+    }
+    const fn sbc16(&mut self, rhs: u16) {
+        let rhs = !rhs;
+        let mut result;
+        if self.decimal() {
+            result = (self.a as u32 & 0x0f) + (rhs as u32 & 0x0f) + self.carry() as u32;
+
+            if result <= 0x0f { result -= 0x06 }
+            result = (self.a as u32 & 0xf0) + (rhs as u32 & 0xf0) + if result > 0x0f { 0x10 } else { 0 } + (result as u32 & 0x0f);
+
+            if result <= 0xff { result -= 0x60 }
+            result = (self.a as u32 & 0xf00) + (rhs as u32 & 0xf00) + if result > 0xff { 0x100 } else { 0 } + (result as u32 & 0xff);
+
+            if result <= 0xfff { result -= 0x600 }
+            result = (self.a as u32 & 0xf000) + (rhs as u32 & 0xf000) + if result > 0xfff { 0x1000 } else { 0 } + (result as u32 & 0xfff);
+        } else {
+            result = self.a as u32 + rhs as u32 + self.carry() as u32;
+        }
+
+        self.set_overflow((!(self.a ^ rhs) & (self.a ^ result as u16) & 0x8000) != 0);
+
+        if self.decimal() && result <= 0xffff {
+            result -= 0x6000
+        }
+
+        self.set_zero_negative16(result as u16);
+        self.set_carry(result > 0xffff);
+
+        self.a = result as u16
+    }
+    const fn sbc(&mut self, rhs: u16) {
+        if self.memory_mode_8() {
+            self.sbc8(rhs as u8)
+        } else {
+            self.sbc16(rhs)
+        }
+    }
+
+    // branch operations
+    const fn bcc(&mut self, rhs: u8) {
+        if !self.carry() { self.bra(rhs) }
+    }
+    const fn bcs(&mut self, rhs: u8) {
+        if self.carry() { self.bra(rhs) }
+    }
+    const fn beq(&mut self, rhs: u8) {
+        if self.zero() { self.bra(rhs) }
+    }
+    const fn bmi(&mut self, rhs: u8) {
+        if self.negative() { self.bra(rhs) }
+    }
+    const fn bne(&mut self, rhs: u8) {
+        if !self.zero() { self.bra(rhs) }
+    }
+    const fn bpl(&mut self, rhs: u8) {
+        if !self.negative() { self.bra(rhs) }
+    }
+    const fn bra(&mut self, rhs: u8) {
+        self.pc = self.pc.wrapping_add(rhs as i8 as u16)
+    }
+    const fn brl(&mut self, rhs: u16) {
+        self.pc = self.pc.wrapping_add(rhs)
+    }
+    const fn bvc(&mut self, rhs: u8) {
+        if !self.overflow() { self.bra(rhs) }
+    }
+    const fn bvs(&mut self, rhs: u8) {
+        if self.overflow() { self.bra(rhs) }
+    }
+
+    // set/clear flag instructions
+    const fn clc(&mut self) {
+        self.set_carry(false)
+    }
+    const fn cld(&mut self) {
+        self.set_decimal(false)
+    }
+    const fn cli(&mut self) {
+        self.set_irq_disable(false)
+    }
+    const fn clv(&mut self) {
+        self.set_overflow(false)
+    }
+    const fn sec(&mut self) {
+        self.set_carry(true)
+    }
+    const fn sed(&mut self) {
+        self.set_decimal(true)
+    }
+    const fn sei(&mut self) {
+        self.set_irq_disable(true)
+    }
+    const fn rep(&mut self, rhs: u8) {
+        self.ps &= !rhs;
+        if self.emulation_mode {
+            self.set_memory_mode_8(true);
+            self.set_index_mode_8(true);
+        }
+    }
+    const fn sep(&mut self, rhs: u8) {
+        self.ps |= rhs;
+        if self.index_mode_8() {
+            self.x &= 0xff;
+            self.y &= 0xff;
+        }
     }
 }
 
